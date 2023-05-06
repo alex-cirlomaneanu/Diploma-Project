2970dadc71ea283728060b9b89179554482d6b53
Alex <acirlomaneanu@stud.acs.upb.ro>
2023-05-06 21:18:34 +0300

    added some login frontend

--- ui/frontend/src/App.js
+++ ui/frontend/src/App.js
@@ -1,14 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
-import Login from "./components/auth/Login";
+import Login from "./componenets/auth/Login";
+import Register from "./componenets/auth/Register";
+import {Route, Routes, Link} from 'react-router-dom';
 
 function App() {
-  return (
-      <div>
-        <h1>Welcome to My App</h1>
-        <Login />
-      </div>
-  );
+    return (
+        <div>
+            <h1>Welcome to HomeBookExpress</h1>
+            <div>
+                <Link to="/login">
+                    <button>Login</button>
+                </Link>
+                <Link to="/register">
+                    <button>Register</button>
+                </Link>
+            </div>
+
+            <Routes>
+                <Route path={'/login'} element={<Login/>}/>
+                <Route path={'/register'} element={<Register/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
