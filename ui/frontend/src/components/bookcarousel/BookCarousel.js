6f33508d4cbba7e00231ede3e3df4b2af4c27b3b
Alex <acirlomaneanu@stud.acs.upb.ro>
2023-06-01 19:10:37 +0300

    updated homepage

---
+++ ui/frontend/src/components/bookcarousel/BookCarousel.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Carousel, Card, Row, Col } from 'react-bootstrap';
+import './BookCarousel.css';
+
+const BookCarousel = ({ books }) => {
+    const bookChunks = chunkArray(books, 3); // Split books into chunks of 3
+
+    return (
+        <Carousel interval={null} indicators={false} variant="dark">
+            {bookChunks.map((chunk, index) => (
+                <Carousel.Item key={index}>
+                    <Row>
+                        {chunk.map((book, innerIndex) => (
+                            <Col key={innerIndex} sm={4}>
+                                <Card className="book-card-carousel bg-transparent">
+                                    {book.bookImage === null ? (
+                                        <Card.Img variant="top" src="/book-cover.jpg" />
+                                    ) : (
+                                        <Card.Img variant="top" src={book.bookImage} />
+                                    )}
+                                    <Card.Body className="book-card-carousel-body">
+                                        <Card.Title className="book-card-carousel-title">{book.title}</Card.Title>
+                                        <Card.Text className="book-card-carousel-text">
+                                            <p>Autor: {book.author.authorName}</p>
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                </Carousel.Item>
+            ))}
+        </Carousel>
+    );
+}
+
+// Helper function to split array into chunks
+function chunkArray(array, chunkSize) {
+    const chunks = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+        chunks.push(array.slice(i, i + chunkSize));
+    }
+    return chunks;
+}
+
+export default BookCarousel;
--- ui/frontend/src/pages/homepage/HomePage.js
+++ ui/frontend/src/pages/homepage/HomePage.js
@@ -3,11 +3,15 @@ import "./HomePage.css";
 import axios from "axios";
 import {AuthContext} from "../../components/auth/auth";
 import {Image} from "react-bootstrap";
+import BookCarousel from "../../components/bookcarousel/BookCarousel";
+import fetchBooks from "../../components/fetchdata/fetchBooks";
 
 const HomePage = () => {
     const userEmail = localStorage.getItem('userEmail');
     const [firstname, setFirstname] = React.useState('');
     const authContext = useContext(AuthContext);
+    const books = fetchBooks();
+
 
     useEffect(() => {
         const fetchData = async () => {
@@ -35,22 +39,12 @@ const HomePage = () => {
             return (
                 <div>
                     <h1 className="greeting">Bine ai revenit, {firstname}</h1>
-                    <Image className={"logo"} src="/logo1.jpg" alt={"logo"}/>
                 </div>
             )
         } else {
             return (
                 <div>
                     <h1 className="greeting">Bine ai venit</h1>
-                    <br/>
-                    <p className="description">Aici vei gasi o multime de carti, de la cele mai cunoscute pana la cele
-                        mai noi.</p>
-                    <p className="description">Intra in contul tau sau creeaza-ti unul pentru a putea comanda cartile
-                        dorite.</p>
-                    <p className="description">Daca nu stii ce sa citesti, poti sa arunci o privire la cartile
-                        recomandate.</p>
-                    <p className={"description"}><Image className={"logo"} src="/logo3.jpg" alt={"logo"}/></p>
-                    <p className={"description"}> Cartile populare </p>
                 </div>
             )
         }
@@ -61,9 +55,27 @@ const HomePage = () => {
             <div className="bookshelf">
                 <Image src="/top-view-books-arrangement.jpg" alt={"bookshelf"} fluid/>
             </div>
-            <div className="div2">
+            <div className="info-div">
                 <br/>
                 <>{Greeting()}</>
+                <br/>
+                <p className="description">Aici vei gasi o multime de carti, de la cele mai cunoscute pana la cele
+                    mai noi.</p>
+                <p className="description">Intra in contul tau sau creeaza-ti unul pentru a putea comanda cartile
+                    dorite.</p>
+                <p className="description">Daca nu stii ce sa citesti, poti sa arunci o privire la cartile
+                    recomandate.</p>
+                <p className={"description"}><Image className={"logo"} src="/logo3.jpg" alt={"logo"}/></p>
+                <p className={"description"}> Cartile populare </p>
+                <div className="popular-books">
+                    <BookCarousel books={books}/>
+                </div>
+            </div>
+            <div className="explore">
+                <a href={"/books"}>
+                    <h3>Nu te-ai hotărât? Explorează cărțile aici</h3>
+                    <img src="/explore.png" alt={"explore"} />
+                </a>
             </div>
         </div>
     );
